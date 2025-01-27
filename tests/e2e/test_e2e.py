708b183ec5b387adf6c019a0e2a08e8587a4ea4f
zyf722 <MaxMixAlex@protonmail.com>
2025-01-27 12:25:50 +0800

    test: add simple project for testing

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
     from cleo.testers.application_tester import ApplicationTester
 
 
-@pytest.mark.parametrize("project", ["poetry18"], indirect=True)
+@pytest.mark.parametrize("project", ["poetry18", "simple-project"], indirect=True)
 def test_poetry_loads_plugin(application_tester: ApplicationTester):
     """Poetry should work with the plugin for test projects."""
     application_tester.execute("migrate -h")
@@ -18,7 +18,7 @@ def test_poetry_loads_plugin(application_tester: ApplicationTester):
     assert "migrate" in application_tester.io.fetch_output()
 
 
-@pytest.mark.parametrize("project", ["poetry18"], indirect=True)
+@pytest.mark.parametrize("project", ["poetry18", "simple-project"], indirect=True)
 @pytest.mark.parametrize("expected_file", ["non-interactive"], indirect=True)
 def test_non_interactive(
     application_tester: ApplicationTester, pyproject_file: Path, expected_file: Path
